0797f7fd7f94603eb6035f33ee50669e2cfbf4b2
majdiShomali <majdi.shomali.1997@gmail.com>
2023-05-09 10:24:48 +0300

    add confirmation

--- beauty-bloom/src/pages/Product/Confirmation.js
+++ beauty-bloom/src/pages/Product/Confirmation.js
@@ -12,7 +12,7 @@ const Confirmation = () => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <h1 class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-pink-600">Thank You !</h1>
-          <p>Thank you for buy from Tech-House ! Check your email for details about product details.</p>
+          <p>Thank you for buy from Beauty bloom ! Check your email  about details.</p>
           <div className='cursor-pointer'>
           <a
             class="inline-flex items-center px-4 py-2 text-white bg-pink-600 border border-pink-600 rounded-full hover:bg-pink-400 focus:outline-none focus:ring">
